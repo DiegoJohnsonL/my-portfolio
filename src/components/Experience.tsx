7d7b1db51d0e3f8722604618b46aa67677dcf538
Diego Johnson <diegojohnson46@gmail.com>
2024-02-22 22:15:52 -0500

    wip

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,9 +1,19 @@
+import { motion } from 'framer-motion';
 import React from 'react'
+import { textVariant } from '../utils/motion';
+import { styles } from '../styles';
+import { StarWrapper } from '../hoc';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+    </>
+  );
 }
 
-export default Experience
+const WrappedExperience = StarWrapper(Experience, 'work');
+export default WrappedExperience;
